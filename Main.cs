e63fdd539dd6498134c4c6786cd91281ec4a6c21
cipher nemo <ciphernemo@users.noreply.github.com>
2022-01-04 20:05:48 -0500

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿#region USING
+using Harmony;
+using System.Reflection;
+#endregion
+
+namespace FastMode
+{
+	public static class Main
+	{
+		//string Directory, string Settings
+		public static void Init()
+		{
+			var harmony = HarmonyInstance.Create("com.github.ciphernemo.FastMode");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
