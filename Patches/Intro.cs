f01e7a8e6e6afbc30874bc94dc011db816ab7e86
cipher nemo <ciphernemo@users.noreply.github.com>
2022-01-04 20:11:31 -0500

    Add files via upload

---
+++ Patches/Intro.cs
@@ -0,0 +1,40 @@
+﻿using BattleTech;
+using Harmony;
+
+namespace FastMode
+{
+	// skip the intro Bink video by setting enum to "HasPlayed" after Init method
+	[HarmonyPatch(typeof(IntroCinematicLauncher))]
+	[HarmonyPatch(nameof(IntroCinematicLauncher.Init), MethodType.Normal)]
+	public static class IntroVideoInit
+	{
+		public static void Postfix()
+		{
+			Traverse.Create(typeof(IntroCinematicLauncher)).Field("state").SetValue(3);
+		}
+	}
+
+	// skip the splash animation by setting serialized activate variable to enabled before Update method
+	[HarmonyPatch(typeof(SplashLauncher), "Update")]
+	public static class SplashAnimationUpdate
+	{
+		public static bool Prefix(SplashLauncher __instance)
+		{
+			var activate = Traverse.Create(__instance).Field("activate").GetValue<ActivateAfterInit>();
+			if (activate != null && !activate.enabled)
+			{
+				activate.enabled = true;
+			}
+			return false;
+		}
+	}
+
+	//[HarmonyPatch(typeof(SplashLauncher), "OnStart")]
+	//public static class SplashAnimationOnStart
+	//{
+	//	public static void Prefix(SplashLauncher __instance)
+	//	{
+	//		Traverse.Create(typeof(SplashAnimationOnStart)).Field("currentState").SetValue("Complete");
+	//	}
+	//}
+}
---
+++ Patches/Travel.cs
@@ -0,0 +1,32 @@
+﻿using BattleTech;
+using Harmony;
+
+namespace FastMode
+{
+	public static class Travel
+	{
+		[HarmonyPatch(typeof(SGTravelManager))]
+		[HarmonyPatch(nameof(SGTravelManager.BeginTransferAnim), MethodType.Normal)]
+		public static class TravelBegin
+		{
+			public static void Postfix(SGTravelManager __instance)
+			{
+				__instance.AnimationInterrupt();
+			}
+		}
+
+		[HarmonyPatch(typeof(SGTravelManager))]
+		[HarmonyPatch(nameof(SGTravelManager.ReturnFromTransferAnim), MethodType.Normal)]
+		public static class TravelReturn
+		{
+			public static void Postfix(SGTravelManager __instance)
+			{
+				var counter = Traverse.Create(__instance).Field("jumpshipChargeAndLeaveAnimCounter").GetValue<int>();
+				if (counter == 1)
+				{
+					__instance.AnimationInterrupt();
+				}
+			}
+		}
+	}
+}
